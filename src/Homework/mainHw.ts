09a8986e59d593730135febaa0bd6a22c5371102
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2024-09-13 22:51:40 +0300

    add

--- src/Homework/mainHw.ts
+++ src/Homework/mainHw.ts
@@ -35,7 +35,7 @@ function showMessage(message: string) {
     console.log(message);
    }
    
-   function calc(num1, num2) {
+   function calc(num1: number, num2: number) {
     return num1 + num2;
    }
    
